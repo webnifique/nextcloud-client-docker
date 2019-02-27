d6f0dfc8fe1bbebd0b5aec7873a9407274a1dd55
Juan Borda <juanignacioborda@gmail.com>
2019-02-26 21:43:23 -0300

    delete sync-folder before sync

--- test.sh
+++ test.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 command -v dialog >/dev/null 2>&1 || { echo >&2 "I require \"dialog\" but it's not installed.  Aborting."; exit 1; }
 
+# delete folder contents
+rm -rf sync-folder/
+# rebuild gitattributes file
+echo "">sync-folder/.gitattributes
+
 password=$(tempfile 2>/dev/null)
 username=$(tempfile 2>/dev/null)
+nextcloudserver=$(tempfile 2>/dev/null)
+dialog --title "Next Cloud Client test" \
+--inputbox "Enter your server url " 10 60 2>$nextcloudserver
+
+ret=$?
+case $ret in
+  0)
+    nextcloudserver=$(<$nextcloudserver);;
+  1)
+    echo "Cancel pressed."
+    exit;;
+  255)
+    [ -s $password ] &&  cat $password || echo "ESC pressed."
+    exit;;
+esac
 
 dialog --title "Next Cloud Client test" \
 --inputbox "Enter your username " 10 30 2>$username
@@ -12,7 +32,8 @@ case $ret in
   0)
     username=$(<$username);;
   1)
-    echo "Cancel pressed.";;
+    echo "Cancel pressed."
+    exit;;
   255)
     [ -s $password ] &&  cat $password || echo "ESC pressed."
     exit;;
@@ -41,5 +62,6 @@ esac
 docker run -it --rm \
  -v $(pwd)/sync-folder:/media/nextcloud \
  -e NC_USER=$username -e NC_PASS=$password \
- -e NC_URL=http://cdn.id.produccion.gob.ar juanitomint/nextcloud-client
+ -e NC_URL=$nextcloudserver\
+ juanitomint/nextcloud-client
  
