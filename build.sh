bfaa6449e35696874ed55aaea3dbeabefa0edada
Juan Borda <juanignacioborda@gmail.com>
2019-02-26 21:29:24 -0300

    added some handy scripts for build push and test

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+time docker build \
+        --build-arg VCS_REF=`git rev-parse --short HEAD` \
+        --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+        -t juanitomint/nextcloud-client . | tee build.log
---
+++ push.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+ docker push juanitomint/nextcloud-client
+# notify microbadger
+curl -X POST https://hooks.microbadger.com/images/juanitomint/nextcloud-client/JElwBprtT-4SrAuHGvuCQjYUO3U=
