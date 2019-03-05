df3d5be2349e85e4dee15ed2f277c7a6f9700a51
Juan Borda <juanignacioborda@gmail.com>
2019-03-05 12:47:20 -0300

    added image size

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ time docker build \
         --build-arg VCS_REF=`git rev-parse --short HEAD` \
         --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
         -t juanitomint/nextcloud-client . | tee build.log
+docker images juanitomint/nextcloud-client
