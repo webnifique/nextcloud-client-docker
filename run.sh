559b529d45ea1e7c8ecb9e9f3a6225927d7b127a
Juan Borda <juanignacioborda@gmail.com>
2019-03-05 10:36:29 -0300

    changed #! /bin/ash for /bin/sh since ash, like sh, is a symlink to /bin/busybox

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/ash
+#!/bin/sh
 
 # ensure, that the sync dir exists and is owned by the user
 [ -d $NC_SYNC_DIR ] || mkdir -p $NC_SYNC_DIR
@@ -12,7 +12,7 @@ do
 	/bin/su -s /bin/ash $USER -c 'nextcloudcmd $( [ $NC_SILENT == true ] && echo "--silent" ) $( [ $NC_TRUST_CERT == true ] && echo "--trust" ) --non-interactive -u $NC_USER -p $NC_PASS $NC_SOURCE_DIR $NC_URL'
 		if [ "$NC_EXIT" = true ] ; then
 			
-			if [  ! "$NC_SILENT" = true ] ; then 
+			if [  ! "$NC_SILENT" == true ] ; then 
 				echo "NC_EXIT is true so exiting... bye!"
 			fi
 			
