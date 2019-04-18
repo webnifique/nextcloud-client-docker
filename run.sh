383f3b14101265d30bcea31f379d39cf8abf76c9
Juan Borda <juanignacioborda@gmail.com>
2019-04-18 18:19:44 -0300

    run sync as root then chown results

--- run.sh
+++ run.sh
@@ -2,14 +2,18 @@
 
 # ensure, that the sync dir exists and is owned by the user
 [ -d $NC_SYNC_DIR ] || mkdir -p $NC_SYNC_DIR
-	if [  ! "$NC_SILENT" = true ] ; then 
-		echo "chown $USER_UID:$USER_GID $NC_SOURCE_DIR";
-	fi
-chown -R $USER:$USER $NC_SOURCE_DIR
+	
 
 while true
 do
-	/bin/su -s /bin/ash $USER -c 'nextcloudcmd $( [ "$NC_SILENT" == true ] && echo "--silent" ) $( [ "$NC_TRUST_CERT" == true ] && echo "--trust" ) --non-interactive -u $NC_USER -p $NC_PASS $NC_SOURCE_DIR $NC_URL'
+
+	nextcloudcmd $( [ "$NC_SILENT" == true ] && echo "--silent" ) $( [ "$NC_TRUST_CERT" == true ] && echo "--trust" ) --non-interactive -u $NC_USER -p $NC_PASS $NC_SOURCE_DIR $NC_URL
+	
+	#chown the files to the USER_UID:
+	echo "chown -R $USER_UID:$USER_GID $NC_SOURCE_DIR";
+	chown -R $USER_UID:$USER_GID $NC_SOURCE_DIR
+	
+	#check if exit!
 		if [ "$NC_EXIT" = true ] ; then
 			
 			if [  ! "$NC_SILENT" == true ] ; then 
