2a89859b8f57f287b96ff2943161b018e0eef02c
webnifique <adm.faurei@gmail.com>
2023-01-20 14:59:36 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 time docker build \
         --build-arg VCS_REF=`git rev-parse --short HEAD` \
         --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-        -t juanitomint/nextcloud-client . | tee build.log
-docker images juanitomint/nextcloud-client
+        -t nextcloud-client . | tee build.log
+docker images nextcloud-client
